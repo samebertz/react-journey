a79b55f40145d62b6f12717f8b2d5516db11458e
Sam Ebertz <samuel.ebertz@gmail.com>
2021-02-10 02:45:09 -0600

    characterList base

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Temp_CharacterListItem from './temp.js'
+// import Temp_CharacterListItem from './temp.js'
 import CharacterListItem from './CharacterListItem.js'
 
 function App() {
+  const mock_CHARACTERS = [
+    'Test', 'Test', 'Test', 'Test', 'Test', 'Test', 'Test', 'Test',
+  ];
   return (
-    <>
-      <Temp_CharacterListItem character="Test"/>
-      <CharacterListItem character='Test' />
-    </>
+    <div className="character-list">
+      {mock_CHARACTERS.map(character => <CharacterListItem key={character} character={character} />)}
+    </div>
   );
+  // return (
+    // <>
+    //   <Temp_CharacterListItem character="Test"/>
+      // <CharacterListItem character='Test' />
+    // </>
+  // );
   // return (
   //   <div className="App">
   //     <header className="App-header">
--- src/CharacterDetail.js
+++ src/CharacterDetail.js
@@ -1,17 +1,22 @@
 import {getAssetPath} from './utils.js';
 
 function CharacterDetail(props) {
-  console.log(Object.keys(props));
-  return Object.keys(props).map(prop => (
-    <ul className="character-detail-list">
-      {props[prop].map(detail => (
-        <li className="character-detail-item">
-          <img className="character-detail-icon" src={getAssetPath(prop, detail)} alt="" />
-          {prop != 'materials' ? <span className="character-detail-label">{detail}</span> : <div></div>}
-        </li>
-      ))}
+  // return ( Object.keys(props).map(prop => (
+  //   <ul className="character-detail-list">
+  //     {props[prop].map(detail => (
+  //       <li className="character-detail-item">
+  //         <img className="character-detail-icon" src={getAssetPath(prop, detail)} alt="" />
+  //       </li>
+  //     ))}
+  //   </ul>
+  // )));
+  return (
+    <ul className="character-material-list">
+      {props.materials.map(material =>
+        <img className="character-material-icon" src={getAssetPath('materials', material)} alt="" />
+      )}
     </ul>
-  ));
+  );
 }
 
 export default CharacterDetail;
